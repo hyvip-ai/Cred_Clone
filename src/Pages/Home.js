b3f4878d538b295ae3e891fab7b773bd24fad933
hyvip-ai <rm2932002@gmail.com>
2021-11-12 16:33:04 +0530

    Added Wrapped Component In home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,8 @@
 import React, { lazy, Suspense } from "react";
 import Spinner from "../Components/Spinner";
-import data from "../Data/ImageText";
 import Mobiles from "../Components/Mobiles";
+const Lower = lazy(()=>import("../parents/Lower"));
 
-const ImageText = lazy(() => import("../Layout/ImageText"));
-const Parallax = lazy(() => import("../Components/Parallax"));
-const Story = lazy(() => import("../Components/Story"));
-const Information = lazy(() => import("../Components/Information"));
-const Rewards = lazy(() => import("../Components/Rewards"));
-const Footer = lazy(() => import("../Components/Footer"));
-const Rating = lazy(() => import("../Components/Rating"));
-const Testimonials = lazy(() => import("../Components/Testimonials"));
 const Upper = lazy(() => import("../parents/Upper"));
 
 function Home() {
@@ -22,16 +14,7 @@ function Home() {
 
       <Mobiles />
       <Suspense fallback={<Spinner />}>
-        <ImageText data={data.deserve_more} />
-        <Rewards />
-        <ImageText data={data.money_matters} />
-        <Parallax />
-        <ImageText data={data.security_first} />
-        <Story />
-        <Rating />
-        <Testimonials />
-        <Information />
-        <Footer />
+        <Lower/>
       </Suspense>
     </>
   );
--- src/parents/Lower.js
+++ src/parents/Lower.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { lazy } from "react";
+import data from "../Data/ImageText";
+
+const ImageText = lazy(() => import("../Layout/ImageText"));
+const Parallax = lazy(() => import("../Components/Parallax"));
+const Story = lazy(() => import("../Components/Story"));
+const Information = lazy(() => import("../Components/Information"));
+const Rewards = lazy(() => import("../Components/Rewards"));
+const Footer = lazy(() => import("../Components/Footer"));
+const Rating = lazy(() => import("../Components/Rating"));
+const Testimonials = lazy(() => import("../Components/Testimonials"));
 
 function Lower() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <>
+      <ImageText data={data.deserve_more} />
+      <Rewards />
+      <ImageText data={data.money_matters} />
+      <Parallax />
+      <ImageText data={data.security_first} />
+      <Story />
+      <Rating />
+      <Testimonials />
+      <Information />
+      <Footer />
+    </>
+  );
 }
 
-export default Lower
+export default Lower;
