7a55415bc5799795151cce6ad51d3dd2a7af7291
hyvip-ai <rm2932002@gmail.com>
2021-11-12 16:27:57 +0530

    AddedWrapped Component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,34 +1,27 @@
 import React, { lazy, Suspense } from "react";
 import Spinner from "../Components/Spinner";
 import data from "../Data/ImageText";
-import Mobiles from '../Components/Mobiles'
+import Mobiles from "../Components/Mobiles";
 
 const ImageText = lazy(() => import("../Layout/ImageText"));
 const Parallax = lazy(() => import("../Components/Parallax"));
 const Story = lazy(() => import("../Components/Story"));
 const Information = lazy(() => import("../Components/Information"));
-const Banner = lazy(() => import("../Components/Banner"));
 const Rewards = lazy(() => import("../Components/Rewards"));
 const Footer = lazy(() => import("../Components/Footer"));
 const Rating = lazy(() => import("../Components/Rating"));
 const Testimonials = lazy(() => import("../Components/Testimonials"));
+const Upper = lazy(() => import("../parents/Upper"));
 
-
-const Navbar = lazy(() => import("../Components/Navbar"));
-const ProgressIndicator = lazy(() =>
-  import("../Components/Progress_Indicator")
-);
 function Home() {
   return (
     <>
-      <Suspense fallback={<Spinner/>}>
-        <ProgressIndicator />
-        <Navbar />
-        <Banner />
-        </Suspense>
+      <Suspense fallback={<Spinner />}>
+        <Upper />
+      </Suspense>
 
-        <Mobiles />
-        <Suspense fallback={<Spinner/>}>
+      <Mobiles />
+      <Suspense fallback={<Spinner />}>
         <ImageText data={data.deserve_more} />
         <Rewards />
         <ImageText data={data.money_matters} />
@@ -40,8 +33,6 @@ function Home() {
         <Information />
         <Footer />
       </Suspense>
-
-    
     </>
   );
 }
---
+++ src/parents/Lower.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Lower() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Lower
