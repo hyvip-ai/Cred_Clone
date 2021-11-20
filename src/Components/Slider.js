f6d02a1c1732b2d9b1342ae6d7e0d442fb0ff52e
hyvip-ai <rm2932002@gmail.com>
2021-11-20 16:24:44 +0530

    Added Small Screen Slider section and made it responsive

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -53,7 +53,7 @@ function Slider() {
   }, [options]);
   return (
     <div className={classes.wrapper}>
-      <div className="row justify-content-center">
+      <div className={`row justify-content-center ${classes.big_screen}`}>
         <div className="col-md-5">
           <section className={`first ${classes.first}`}>
             <h1>
@@ -91,7 +91,7 @@ function Slider() {
             <p>
               switch to CRED RentPay and start paying rent with your credit
               card. this way you get up to 45 days of credit free period, more
-              reward points and a happy landlord.F
+              reward points and a happy landlord.
             </p>
           </section>
         </div>
@@ -106,6 +106,106 @@ function Slider() {
                 alt="house"
               />
             </div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.small_screen}>
+        <div className="first">
+          <div className={classes.data}>
+            <h1>
+              we’ve got <br /> your back.
+            </h1>
+          </div>
+          <div className="image">
+            <div className={classes.mobile_outer}>
+              <div className={classes.mobile_inner}>
+                <img
+                  src="https://web-images.credcdn.in/_next/assets/images/home-page/features/fold1.png"
+                  alt="House"
+                />
+              </div>
+            </div>
+          </div>
+          <div className={classes.data}>
+            <p>
+              gain complete control over your credit card with CRED Protect.
+              receive category-based analysis of your spends, detect hidden
+              charges, and track your credit limit in real-time.
+            </p>
+          </div>
+        </div>
+
+        <div className="second">
+          <div className={classes.data}>
+            <h1>
+              begin your <br /> winning streak.
+            </h1>
+          </div>
+          <div className="image">
+            <div className={classes.mobile_outer}>
+              <div className={classes.mobile_inner}>
+                <img
+                  src="https://web-images.credcdn.in/_next/assets/images/home-page/features/fold1.png"
+                  alt="House"
+                />
+              </div>
+            </div>
+          </div>
+          <div className={classes.data}>
+            <p>
+              use your CRED coins to participate in games and raffles to win the
+              most exclusive rewards and cashbacks on CRED. good luck.
+            </p>
+          </div>
+        </div>
+
+        <div className="third">
+          <div className={classes.data}>
+            <h1>
+              for your <br /> eclectic taste.
+            </h1>
+          </div>
+          <div className="image">
+            <div className={classes.mobile_outer}>
+              <div className={classes.mobile_inner}>
+                <img
+                  src="https://web-images.credcdn.in/_next/assets/images/home-page/features/fold1.png"
+                  alt="House"
+                />
+              </div>
+            </div>
+          </div>
+          <div className={classes.data}>
+            <p>
+              get access to the CRED Store, a member-exclusive selection of
+              products and experiences at special prices that complement your
+              taste. this is the good life they speak of.
+            </p>
+          </div>
+        </div>
+
+        <div className="fourth">
+          <div className={classes.data}>
+            <h1>
+              more cash <br /> in your pockets.
+            </h1>
+          </div>
+          <div className="image">
+            <div className={classes.mobile_outer}>
+              <div className={classes.mobile_inner}>
+                <img
+                  src="https://web-images.credcdn.in/_next/assets/images/home-page/features/fold1.png"
+                  alt="House"
+                />
+              </div>
+            </div>
+          </div>
+          <div className={classes.data}>
+            <p>
+              switch to CRED RentPay and start paying rent with your credit
+              card. this way you get up to 45 days of credit free period, more
+              reward points and a happy landlord.
+            </p>
           </div>
         </div>
       </div>
